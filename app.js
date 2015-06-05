0be7c0477178428267f52c6577acf5edde171a94
chenchocs <chenchomail@gmail.com>
2015-06-05 21:50:27 +0100

    Funcionalidad básica de la aplicación lista

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+var partials = require('express-partials');
+var routes = require("./routes/index");
 
 var app = express();
 
@@ -14,14 +14,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(partials());
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+var indexController = require ("../controllers/index_controller");
+var quizController = require ("../controllers/quiz_controller");
+
+router.get("/", indexController.index);
+router.get("/quizes/question", quizController.question);
+router.get("/quizes/answer", quizController.answer);
 
 module.exports = router;
